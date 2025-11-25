0cc6ac4b01d422b116f673d0dec8e975c00c895a
Mateo Pavoni <mateopavoni905@gmail.com>
2025-11-25 19:55:11 -0300

    feat: agregue vista de crear cuenta y le agregue sus estilos

--- views/auth/crear.php
+++ views/auth/crear.php
@@ -1,1 +1,60 @@
-<h1>Crea tu cuenta</h1>
+<div class="contenedor crear">
+    <?php include_once __DIR__ . "/../templates/nombre-sitio.php" ?>
+
+    <div class="contenedor-sm">
+        <p class="descripcion-pagina">Crea tu cuenta en UpTask</p>
+        <form class="formulario" method="POST" action="/">
+            <div class="campo">
+                <label for="nombre">Nombre</label>
+                <input
+                    type="text"
+                    id="nombre"
+                    placeholder="Tu nombre"
+                    name="nombre"
+                />
+            </div>
+            <div class="campo">
+                <label for="apellido">Apellido</label>
+                <input
+                    type="text"
+                    id="apellido"
+                    placeholder="Tu apellido"
+                    name="apellido"
+                />
+            </div>
+            <div class="campo">
+                <label for="email">Email</label>
+                <input
+                    type="email"
+                    id="email"
+                    placeholder="Tu email"
+                    name="email"
+                />
+            </div>
+            <div class="campo">
+                <label for="password">Password</label>
+                <input
+                    type="password"
+                    id="password"
+                    placeholder="Tu password"
+                    name="password"
+                />
+            </div>
+            <div class="campo">
+                <label for="password2">Repetir password</label>
+                <input
+                    type="password"
+                    id="password2"
+                    placeholder="Repite tu password"
+                    name="password2"
+                />
+            </div>
+            <input type="submit" class="boton" value="Iniciar Sesión" />
+        </form>
+
+        <div class="acciones">
+            <a href="/crear">¿Ya tienes una cuenta? Inicia sesión</a>
+            <a href="/olvide">¿Olvidaste tu contraseña? Recupera el acceso de tu cuenta aquí</a>
+        </div>
+    </div>
+</div>
