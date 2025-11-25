6f5586ad533de0628ecdc447c7044ff033de6555
Mateo Pavoni <mateopavoni905@gmail.com>
2025-11-25 12:33:44 -0300

    chore: configure conexión de la bbdd desde el .env

--- includes/database.php
+++ includes/database.php
@@ -1,10 +1,20 @@
 <?php
 
-$db = mysqli_connect('localhost', '', '', '');
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
+$dotenv->load();
+
+$db = mysqli_connect(
+    $_ENV['DB_HOST'],
+    $_ENV['DB_USER'],
+    $_ENV['DB_PASS'],
+    $_ENV['DB_NAME']
+);
 
 if (!$db) {
-    echo "Error: No se pudo conectar a MySQL.";
-    echo "errno de depuración: " . mysqli_connect_errno();
-    echo "error de depuración: " . mysqli_connect_error();
+    echo "Error: No se pudo conectar a MySQL.<br>";
+    echo "errno de depuración: " . mysqli_connect_errno() . "<br>";
+    echo "error de depuración: " . mysqli_connect_error() . "<br>";
     exit;
 }
