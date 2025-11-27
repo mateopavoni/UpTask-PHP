903d5d0c05bfe9542718ccd60287b78add76ea37
Mateo Pavoni <mateopavoni905@gmail.com>
2025-11-26 21:45:36 -0300

    feat: agregue la ruta y el controlador del Dashboard

--- public/index.php
+++ public/index.php
@@ -2,6 +2,7 @@
 
 require_once __DIR__ . '/../includes/app.php';
 
+use Controllers\DashboardController;
 use MVC\Router;
 use Controllers\LoginController;
 $router = new Router();
@@ -21,6 +22,8 @@ $router->post("/reestablecer", [LoginController::class, "reestablecer"]);
 
 $router->get("/mensaje", [LoginController::class, "mensaje"]);
 $router->get("/confirmar", [LoginController::class, "confirmar"]); 
+
+$router->get("/dashboard",[DashboardController::class, "index"]);
 
 
 // Comprueba y valida las rutas, que existan y les asigna las funciones del Controlador
